440e8ba08e51f4cc749391e099b35f949aea3808
Kyle <140885143+kyle-gardner@users.noreply.github.com>
2024-04-19 04:16:18 -0400

    okay, it was NOT working before and now it is as i expect it to. idk if its shunting yard anymore

--- src/tree.rs
+++ src/tree.rs
@@ -13,7 +13,7 @@ struct OpPrecedence {
 	associativity: bool, //left = false, right = true
 }
 
-pub fn create_tree(token_vec: Vec<Token>) {
+pub fn create_tree(mut token_vec: Vec<Token>) {
 	let tree = BinaryTree{left: Option::None, right: Option::None};
 
 	let mut precedence_hm: HashMap<&str, OpPrecedence> = HashMap::new();
@@ -24,31 +24,28 @@ pub fn create_tree(token_vec: Vec<Token>) {
 	precedence_hm.insert("=", OpPrecedence { prec: 1, associativity: false });
 	// should not be done
 	precedence_hm.insert("(", OpPrecedence { prec: 0, associativity: false });
+	precedence_hm.insert(";", OpPrecedence { prec: 0, associativity: false});
 
 	// shunting yard
+	// i dont know if i can call this shunting yard anymore. im not following the algo
 	let mut op_stack: Vec<Token> = Vec::new();
 	let mut output: Vec<Token> = Vec::new();
 
+	token_vec.reverse();
+
 	for t in token_vec {
 		if t.kind == Kind::Operator {
-			// only if there is something in ops
-			if let Some(op) = op_stack.pop() {
-				// check precedence
-				let op_in_stack = precedence_hm.get(op.content.as_str()).unwrap();
-				let new_op = precedence_hm.get(t.content.as_str()).unwrap();
-
-				// if precedence of t is >, we push to ops,
-				// else, pop op off stack and add to output, then push t onto stack
-				if new_op.prec > op_in_stack.prec {
-					op_stack.push(op);
-					op_stack.push(t);
+			//let t_content = t.content.as_str();
+			// only if there is something in op_stack
+			while let Some(op_in_stack) = op_stack.pop() {
+				if precedence_hm.get(op_in_stack.content.as_str()).unwrap().prec > precedence_hm.get(t.content.as_str()).unwrap().prec {
+					output.push(op_in_stack);
 				} else {
-					output.push(op);
-					op_stack.push(t);
+					op_stack.push(op_in_stack);
+					break;
 				}
-			} else {
-				op_stack.push(t);
 			}
+			op_stack.push(t);	
 		} else if t.kind == Kind::Seperator {
 			match t.content.as_str() {
 				"(" => op_stack.push(t),
@@ -61,7 +58,8 @@ pub fn create_tree(token_vec: Vec<Token>) {
 						}
 					}
 				},
-				";" => op_stack.push(t),
+				// ignore ; not needed 
+				";" => (),
 				_ => {}
 			}
 		} else {
